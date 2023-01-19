cc984e0258be6f30a33f35b67349e5cf919886d2
Jose Sissa <jsissame3484@universidadean.edu.co>
2023-01-19 11:50:59 -0500

    Conexión db

---
+++ connection.php
@@ -0,0 +1,34 @@
+<?php 
+
+// require __DIR__ . '/vendor/autoload.php';
+
+// $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+// $dotenv->load();
+
+class Connection {
+    public function obtDatos() {
+        $server = "localhost";
+        $user = "root";
+        $password = "";
+        $database = "diagnosticocctn";
+
+        $con = mysqli_connect($server, $user, $password, $database);
+
+        if ($con->connect_error) {
+            die("Error al conectar con la db: " . $con->connect_error);
+        }
+        $query = "SELECT * FROM ciudades ORDER BY ciudad ASC";
+        $datos = mysqli_query($con, $query);
+
+        while($row = mysqli_fetch_assoc($datos)){
+            $json[] = $row;
+        };
+
+        if ($datos == false) {
+            return -1;
+		}
+		
+        return $json;
+        // return json_encode($json);
+    }
+};
